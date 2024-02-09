04d1e37454b8ad29a2730ceb644a6ca4120fb096
VicoshExee <157852203+VicoshExee@users.noreply.github.com>
2024-02-09 11:14:17 +0100

    Update Character.cpp

--- Katana/Character/Character.cpp
+++ Katana/Character/Character.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
+#include <vector> 
 #include "Character.h"
 
-Character::Character(CharacterName name, int HP) : name(name), HP(HP) {}
+
+std::vector<Character> characters;
+
+Character::Character(CharacterName name, int HP) {
+    this->name = name;
+    this->HP = HP;
+}
+
 
 CharacterName Character::getName() {
     return name;
 }
 
-int Character::getHP() {
-    return HP;
-}
 
 void Character::print() {
     std::cout << "Name: " << static_cast<int>(name) << std::endl;
     std::cout << "HP: " << HP << std::endl;
+}
+
+
+void setHP() {
+    for (int i = 0; i < 12; i++) { 
+        if (i <= 4) {
+            characters.push_back(Character(static_cast<CharacterName>(i), 4)); 
+        } else {
+            characters.push_back(Character(static_cast<CharacterName>(i), 5)); 
+        }
+    }
 }
